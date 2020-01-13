54a12c12828f18d4bf4244e639fc0cf35e3dc7e4
simsmat116 <simsmat11697@gmail.com>
2020-01-12 21:03:20 -0500

    Implemented the page selection feature for navigational purposes. Next step is to figure out how to get link to product and potentially product images.

--- consumer-scraper/templates/static/js/components/PageNav.jsx
+++ consumer-scraper/templates/static/js/components/PageNav.jsx
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 
 class PageNav extends Component {
   render(){
-    return (
+    let currPage = this.props.currPage;
+    let numPages = this.props.numPages;
+    let maxPage = Math.min(numPages, currPage + 5);
+
+    let pages = []
+    // If the first page isn't displayed, give user a button to click to go back
+    if(currPage != 1){
+      pages.push(<button class="page" onClick={this.props.onClicked}>1</button>);
+      pages.push(<div class="page-dots">...</div>);
+    }
+
+    // Add buttons for the next five pages
+    for(let i = currPage; i <= maxPage; ++i){
+      pages.push(<button class="page" onClick={this.props.onClicked}>{i}</button>);
+    }
+
+    // If the last page isn't displayed, give user a button to click to go to end
+    if(numPages - currPage > 4){
+      pages.push(<div class="page-dots">...</div>);
+      pages.push(<button class="page" onClick={this.props.onClicked}>numPages</button>);
+    }
+
+    return(
       <div class="page-bar">
-        <button class="page">1</button>
-        <button class="page">1</button>
-        <button class="page">1</button>
-        <button class="page">1</button>
+        {pages}
       </div>
+
     )
   }
 }
--- consumer-scraper/templates/static/js/components/ProductSearch.jsx
+++ consumer-scraper/templates/static/js/components/ProductSearch.jsx
@@ -9,9 +9,10 @@ class ProductSearch extends Component {
   constructor(props) {
     // Initialize mutable state
     super(props);
-    this.state = { products: [], search: "", page: 1 };
+    this.state = { products: [], search: "", page: 1, numPages: 0 };
     this.handleSearch = this.handleSearch.bind(this);
     this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.pageClick = this.pageClick.bind(this);
   }
 
   handleSearchChange(e){
@@ -31,11 +32,34 @@ class ProductSearch extends Component {
       .then((context) => {
           this.setState({
             products: context.results,
+            numPages: context.num_pages
           });
-
-
       })
       .catch(error => console.log(error));
+  }
+
+  pageClick(e){
+    let newPage = parseInt(e.currentTarget.textContent, 10)
+    // Set the page to be the page that the user clicked on
+    this.setState({
+      page: newPage
+    });
+    // Send a request to the backend to get products for the certain page
+    const url = "api/search?q=" + this.state.search + "&p=" + String(e.currentTarget.textContent);
+    console.log(url);
+    fetch(url, { credentials: 'same-origin' })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((context) => {
+          this.setState({
+            products: context.results,
+            numPages: context.num_pages
+          });
+      })
+      .catch(error => console.log(error));
+      window.scrollTo(0, 0)
   }
 
   render() {
@@ -49,7 +73,7 @@ class ProductSearch extends Component {
         {this.state.products.map(product => (
           <Product price={product.price} productDesc={product.product_desc} />
         ))}
-        <PageNav />
+        <PageNav onClicked={this.pageClick} numPages={this.state.numPages} currPage={this.state.page} />
       </div>
     );
   }
