486b13c3b23dac67eb53eafa5dec7474cbb4c75d
simsmat116 <simsmat11697@gmail.com>
2020-01-13 23:26:05 -0500

    Worked on obtaining new information from the scrape results using BeautifulSoup. I was able to find image links, product name, and product link

--- consumer-scraper/templates/search/scraper.py
+++ consumer-scraper/templates/search/scraper.py
@@ -1,6 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
+def getProductLink(google_link):
+    # When the google_link contains "http.." then it's already the product link
+    if "http" == google_link[0:4]:
+        return google_link
+    # Have to add beginning of the url in case of google_link being "/shopping/prod.."
+    url = "https://www.google.com" + google_link
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    # The link is contained in the "Visit site" link
+    elem = soup.find("a", string="Visit Site", href=True)
+    if not elem:
+        return ""
+
+    return elem["href"]
+
 def scrape_search_results(search, db):
     # Url for Google Shopping
     url = "https://www.google.com/search?psb=1&tbm=shop&q=" + search
@@ -14,28 +29,36 @@ def scrape_search_results(search, db):
     # Loop through the results to get price and product description
     for product in soup.findAll("div", attrs={"class": "pslires"}):
         count = 0
-        product_desc,  price  = "", ""
+        product_desc, product_name, price, image, link = "", "", "", "", ""
         for element in product:
-            if count == 1:
+            if count == 0:
+                image = element.find("a").find("img", src=True)["src"]
+            elif count == 1:
                 # Get the price and exclude first character ($)
                 price = element.find("div").text[1:]
                 # Sometimes spaces are followed by text, need to remove to convert to float
                 if price.find(" ") > 0:
                     price = price[:price.find(" ")]
-                # Need to remove comma before converting to float
+                    # Need to remove comma before converting to float
                 price = float(price.replace(",", ""))
             elif count == 2:
+                link = element.find("a", href=True)
+                product_name = link.text
+                link = getProductLink(link["href"])
                 product_desc = element.find("div").text
+
             count += 1
 
-            # Only insert products that have both price and description
-            if price and product_desc:
+            foundAll = product_desc and product_name and price and image and link
+
+            if foundAll:
                 cursor = db.cursor()
                 insert_info = (search, price, product_desc)
                 # Add the product info to the database
                 cursor.execute("INSERT INTO results (search, price, product) VALUES(%s, %s, %s)", insert_info)
                 db.commit()
                 results.append((product_desc, price))
+                
     results.sort(key=lambda tup: tup[1])
 
     return results[:10]
