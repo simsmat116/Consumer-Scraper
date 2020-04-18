edd685a5714c95fbb65e1e393a00847a44251b34
simsmat116 <simsmat11697@gmail.com>
2020-04-17 21:10:40 -0400

    Beginning to refactor the css using bootstrap. This will allow for faster front-end development and learning a new framework (:. It's definitely good to be back working on this project. So far, I have only refactored the navbar.

--- consumer-scraper/templates/static/js/components/NavBar.jsx
+++ consumer-scraper/templates/static/js/components/NavBar.jsx
@@ -21,27 +21,50 @@ class NavBar extends Component {
   render(){
     // Determine if a user is logged in
     let topRight;
-    if(!localStorage.getItem('username')){
-      topRight = <LoginPopup success={this.handleLoginSuccess} />
+    if(localStorage.getItem('username')){
+      topRight = (
+        <ul class="nav navbar-nav navbar-right">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Username <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Profile</a></li>
+              <li><a href="#">Messages</a></li>
+              <li><a href="#">Settings</a></li>
+              <li><a href="#">Logout</a></li>
+            </ul>
+        </li>
+
+        </ul>
+
+      )
     }
     else{
       topRight = (
-                    <div class="login">{localStorage.getItem('username')}
-                      <div class="loginDropMenu">
-                        <div>Your profile</div>
-                        <div>Your message</div>
-                        <div onClick={this.handleLogout}>Logout</div>
-                      </div>
-                    </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href=""><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
                  );
     }
 
     return(
-      <div class="topnav">
-        <a href='/'>Home</a>
-        <a href='/popular_products'>Popular Products</a>
-        {topRight}
-      </div>
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Consumer Scraper</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class><a href="/">Home</a></li>
+            <li><a href="/popular_products">Popular Products</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href=""><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
